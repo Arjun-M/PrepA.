ff6d3eda4bbac9cf33e4730a0a681b5c39eeaa1a
Arjun <86394506+Arjun-M@users.noreply.github.com>
2024-09-02 14:51:19 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.set('view engine', 'ejs');
+app.use( express.json() )
+app.use( express.urlencoded({ extended: true }) )
+app.use( express.static("PrepA/Template") )
+app.set('views', path.join(__dirname, "/PrepA/Template"))
+
+app.use('/', require('./PrepA/Server/Router.js'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
